4fd791f1b49162cba2465dfa805372dfae132237
apanariello4 <nellopanariello96@gmail.com>
2022-10-13 15:02:04 +0200

    data path in argparse

--- datasets/seq_ubnormal.py
+++ datasets/seq_ubnormal.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import hashlib
+from typing_extensions import Self
 import warnings
 from argparse import Namespace
 from pathlib import Path
